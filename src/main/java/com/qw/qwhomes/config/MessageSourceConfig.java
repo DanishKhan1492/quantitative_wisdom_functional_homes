89813610f4fbac620b1a9d56dad0159702f2c89e
Muhammad Danish Khan <danishkhan@protonmail.ch>
2025-03-02 16:16:34 +0500

    issue with incorrect message fixed

--- src/main/java/com/qw/qwhomes/config/MessageSourceConfig.java
+++ src/main/java/com/qw/qwhomes/config/MessageSourceConfig.java
@@ -3,15 +3,15 @@ package com.qw.qwhomes.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 public class MessageSourceConfig {
 
     @Bean
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("i18n/messages");
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
