e72b4bb8719c185043982c8fa5f1375bfef74075
Abuzar Khan <105778612+abuzarkhan1@users.noreply.github.com>
2025-10-12 15:42:58 +0500

    discount added

--- src/main/java/com/qw/qwhomes/domains/proposal/service/mapper/ProposalMapper.java
+++ src/main/java/com/qw/qwhomes/domains/proposal/service/mapper/ProposalMapper.java
@@ -34,6 +34,7 @@ public interface ProposalMapper {
     @Mapping(target = "name", source = "product.name")
     @Mapping(target = "sku", source = "product.sku")
     @Mapping(target = "productId", source = "product.productId")
+    @Mapping(target = "supplierDiscount", source = "product.supplier.supplierDiscount")
     ProposalProductDTO toDto(ProposalProduct entity);
 
 
